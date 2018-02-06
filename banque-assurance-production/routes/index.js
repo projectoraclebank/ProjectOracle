4fb1e550f6f2e39bfcbfddd636c6b1cbee8ca676
Gilbert <gilbert.torchon@esih.edu>
2018-02-06 18:37:24 -0500

    Login et recuperation du nom du client

--- banque-assurance-production/routes/index.js
+++ banque-assurance-production/routes/index.js
@@ -4,7 +4,7 @@ var database=require("../config/database");
 /* GET home page. */
 var session;
 router.get('/', function(req, res, next) {
-   res.render('index');
+  res.render('index');
 });
 
 
@@ -22,20 +22,25 @@ router.post('/login',function(req,res){
   session=req.session;
   var pseudo=req.body.pseudo_client;
   var password=req.body.password;
+  console.log('PSEUDO: '+pseudo+' PASSWORD: '+password);
   var sql="SELECT * FROM user_client WHERE pseudo_client=? AND password_client=?";
   database.query(sql,[pseudo,password],function(err,result){
-    if(err) throw err;
-    if(result.length>0)
-    {
-      // var id_client=result.id_user_client;
-      console.log('RESULT'+JSON.stringify(result));
-      session.uniqueID=result[0].id_user_client;
-      console.log("SESSION: "+JSON.stringify(session.uniqueID));
-      session.userData=result[0];
-      res.redirect('/client/dashboard_client');
+    if(err){
+      console.log("ERREUR :\n"+err);
     }
     else{
-      res.render('client',{message:'Rentrer un compte valide',className:'alert alert-danger'})
+      if(result.length>0)
+      {
+        // var id_client=result.id_user_client;
+        console.log('RESULT'+JSON.stringify(result));
+        session.uniqueID=result[0].id_user_client;
+        console.log("SESSION: "+JSON.stringify(session.uniqueID));
+        session.userData=result[0];
+        res.redirect('/client/dashboard_client');
+      }
+      else{
+        res.render('client',{message:'Rentrer un compte valide',className:'alert alert-danger'})
+      }
     }
   });
 
@@ -63,14 +68,14 @@ router.get('/tables',function(req,res){
 });
 
 router.get('/client/dashboard_client',function(req,res,next){
-   var userData="";
-   if(session.uniqueID)
-   {
-     console.log("SESSION ID:"+session.uniqueID);
-     console.log("IM INSIDE!: ");
-     userData=session.userData;
-   }
-   res.render('dashboard_client',{userData:userData});
+  var userData="";
+  if(session.uniqueID)
+  {
+    console.log("SESSION ID:"+session.uniqueID);
+    console.log("IM INSIDE!: ");
+    userData=session.userData;
+  }
+  res.render('dashboard_client',{userData:userData});
 })
 
 module.exports = router;
